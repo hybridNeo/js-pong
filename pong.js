104802f17f574ca9df3cd859bac3c0a3b9686a54
hybridNeo <rahul.mahadev7@gmail.com>
2015-02-11 00:03:01 +0530

    First Commit

---
+++ pong.js
@@ -0,0 +1,88 @@
+count = 0;
+a=121;
+function hit(){
+	//document.write("hit");
+	dx = -dx;
+	dy = -dy;
+	//inteval--;
+}
+function out(){
+	x=500;
+ 	y=400;
+	//score = document.getElementById("score");
+	var a  = parseInt(score.innerHTML(),10);
+	score.innerHTML() = a--;
+	//alert(a);
+}
+function move(){
+	var bat_y = parseInt(bat1.style.top,10);
+	var text_y = parseInt(text.style.top,10);
+	var text_x = parseInt(text.style.left,10); 
+	if(x==900){
+		hit();
+	}
+	if(x==10 && y>a && y<=(a+100000) ){
+		hit();
+		//document.write('here');	
+	}
+	if(x > 900){
+		out();
+	}
+	//document.write(text_x);
+	if(x < 10){
+		out();	
+		//document.write(a);
+		
+	}	
+	/*
+	if(parseInt(text.style.left,10)==10 && (parseInt(text.style.top,10) > parseInt(bat1.style.top,10))){
+		hit();	
+		alert("win");
+	}
+	//alert('hello');
+	///document.write( parseInt(bat1.style.left,10) + " and "+ parseInt(text.style.left,10));
+	if(parseInt(text.style.left,10) < 10)
+		out();
+	*/
+	if(x+dx > (screen.availWidth -w) || (x+dx<0)){
+		dx = -dx;
+	}
+	if(y+dy > (screen.availHeight -h) || (y+dy<0)){
+		dy = -dy;
+	}
+		x+=dx;
+		y+=dy;
+		text.style.position = "absolute";
+		text.style.left = x;
+		text.style.top = y;
+	if(count%3==0){
+		text.style.color="red";
+	}else if(count%2==0){
+		text.style.color="green";
+	}else{
+		text.style.color="blue";
+	}
+	count++;
+}
+function follow(event){
+		ev=event||window.event; //get event info
+		bat1.style.position="absolute";
+		bat2.style.position="absolute";
+		//text.style.left = ev.clientX +2 + "px";
+		a = ev.clientY;
+		bat1.style.top = ev.clientY ;
+		bat2.style.top = ev.clientY ;
+}
+function start(){
+	//alert('hello');
+	document.addEventListener("mousemove",follow,false);
+	x=0,y=0,w=200,h=200;
+	text = document.getElementById("text");
+	bat1 = document.getElementById("bat1");
+	bat2 = document.getElementById("bat2");
+	dx=5,dy=5;
+	interval =10;
+	id_timeout = setInterval(move,interval);
+	
+	
+}
